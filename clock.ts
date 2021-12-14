1ec627ea69caa6617dfb49f8b0d826a2bcfb9f8a
DiamondYuan <admin@diamondyuan.com>
2021-12-14 08:29:56 +0800

    feat: add crdt clock

---
+++ clock.ts
@@ -0,0 +1,89 @@
+// copy from https://github.com/codesandbox/crdt-tree/blob/eef85782af0949dd5f3dcc30ee0e8913dcb5ae44/src/Clock.ts
+
+// MIT License
+
+// Copyright (c) 2021 Matan Kushner
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+export enum Ordering {
+  Equal,
+  Greater,
+  Less,
+}
+
+/** Implements a Lamport Clock */
+export class Clock<Id> {
+  actorId: Id;
+  counter: number;
+
+  constructor(actorId: Id, counter: number = 0) {
+    this.actorId = actorId;
+    this.counter = counter;
+  }
+
+  /** Returns a new Clock with same actor but counter incremented by 1 */
+  inc(): Clock<Id> {
+    return new Clock(this.actorId, this.counter + 1);
+  }
+
+  /** Increments the clock counter and returns a new Clock */
+  tick(): Clock<Id> {
+    this.counter += 1;
+    return new Clock(this.actorId, this.counter);
+  }
+
+  /** Returns a new clock with the same actor but the counter is the larger of the two */
+  merge(clock: Clock<Id>): Clock<Id> {
+    return new Clock(this.actorId, Math.max(this.counter, clock.counter));
+  }
+
+  /** Compare the ordering of the current Clock with another */
+  compare(other: Clock<Id>): Ordering {
+    // Compare Clock's counter with another
+    if (this.counter > other.counter) return Ordering.Greater;
+    if (this.counter < other.counter) return Ordering.Less;
+
+    // If counters are equal, order is determined based on actorId
+    // (this is arbitrary, but deterministic)
+    if (this.actorId > other.actorId) return Ordering.Greater;
+    if (this.actorId < other.actorId) return Ordering.Less;
+    return Ordering.Equal;
+  }
+
+  /**
+   * Used to retreive a value's primitive, used in comparisons
+   * @example
+   * const clock1 = new Clock('a');
+   * const clock2 = new Clock('b');
+   * clock1.tick();
+   *
+   * // returns true
+   * console.log(clock1 > clock2);
+   */
+  valueOf(): string {
+    return this.toString();
+  }
+
+  /** Stringify the current clock into a comparable string */
+  toString(): string {
+    const paddedCounter = String(this.counter).padStart(10, "0");
+    return `${paddedCounter}:${this.actorId}`;
+  }
+}
